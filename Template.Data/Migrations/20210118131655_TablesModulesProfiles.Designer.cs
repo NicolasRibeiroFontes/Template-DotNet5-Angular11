688829f45149e81eedcb4cd71329fe5f0f7e4d6d
Nicolas Fontes <nicolas.rfontes@gmail.com>
2021-01-18 13:28:32 +0000

    Migration TablesModulesProfiles

--- Template.Data/Migrations/20210118131655_TablesModulesProfiles.Designer.cs
+++ Template.Data/Migrations/20210118131655_TablesModulesProfiles.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Template.Data.Context;
 
 namespace Template.Data.Migrations
 {
     [DbContext(typeof(MySQLContext))]
-    partial class MySQLContextModelSnapshot : ModelSnapshot
+    [Migration("20210118131655_TablesModulesProfiles")]
+    partial class TablesModulesProfiles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
