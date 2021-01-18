a4fc88b6e46f1b3984d31ba2490a1e228234044d
Nicolas Fontes <nicolas.rfontes@gmail.com>
2021-01-18 12:02:41 +0000

    Controllers - Web Project

---
+++ Template/Controllers/ModulesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Template.Controllers
+{
+	[Route("api/[controller]"), ApiController]
+	public class ModulesController : ControllerBase
+	{
+	}
+}
