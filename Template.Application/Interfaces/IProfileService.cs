338782f1229c0b49d2c6c95decd32112e0d87d84
Nicolas Fontes <nicolas.rfontes@gmail.com>
2021-01-18 11:52:23 +0000

    Application Project

---
+++ Template.Application/Interfaces/IProfileService.cs
@@ -0,0 +1,6 @@
+﻿namespace Template.Application.Interfaces
+{
+	public interface IProfileService
+	{
+	}
+}
---
+++ Template.Application/Services/ProfileService.cs
@@ -0,0 +1,8 @@
+﻿using Template.Application.Interfaces;
+
+namespace Template.Application.Services
+{
+	public class ProfileService: IProfileService
+	{
+	}
+}
